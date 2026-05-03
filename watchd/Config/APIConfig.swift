1d72110beea4585c380224a25dc7c9a66a9c6c92
sentinel-dot <nikola-milinkovic@hotmail.com>
2026-05-03 20:53:33 +0200

    phase 11 done refactor

--- watchd/Config/APIConfig.swift
+++ watchd/Config/APIConfig.swift
@@ -7,7 +7,7 @@ enum APIConfig {
     #if DEBUG
     private static let backendBaseURL = "http://localhost:3000"
     #else
-    private static let backendBaseURL = "http://192.168.178.33:3000"
+    private static let backendBaseURL = "https://watchd.up.railway.app"
     #endif
 
     static var baseURL: String { "\(backendBaseURL)/api" }
